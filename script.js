1939c3e9d604b09893a86bc998a4999c0b223e69
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-09-27 23:59:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+function updateTimer(){
+	const time=document.getElementById("timer");
+	const currentdate=new Date();
+
+	const formatted=currentdate.toLocaleTimeString();
+
+	time.textcontent=formatted;
+}
+updateTimer();
+setInterval(updateTimer,1000)
